445b7d1e84123e5e6d80188c1d7ab8a833eb4942
chenchenyangll <ccy-8888@hotmail.com>
2015-01-09 22:04:15 +0800

    add wiki.js

---
+++ wiki.js
@@ -0,0 +1,26 @@
+module.exports = (function() {
+  var bot = require('nodemw');
+  
+  var wiki = function(options) {
+    this.config = config = options.config;
+    /* 
+     * config is an object by default, 
+     * we assume the account is a bot
+     */
+     this.client = new bot(config);
+   };
+   
+   wiki.prototype = {
+     tryBot: function(callback) {
+       if (this.isLogin !== true) {
+         var that = this;
+         this.client.logIn(function() {
+           that.isLogin = true;
+           callback();
+         });
+       }
+     }
+   };
+   
+   return wiki;
+ }());
