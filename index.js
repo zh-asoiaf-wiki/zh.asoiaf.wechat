5547ff1ca3c03b3cbff5ebf30c0e1d88b26d3fcb
AemonTargaryen <ccy-8888@hotmail.com>
2015-01-10 17:05:44 +0800

    handle subscribe event

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var wcConf = {
 };
 
 var utility = require('zh.asoiaf.utility');
+/*
 var wiki = new utility.Wiki({
   config: {
     "server": "zh.asoiaf.wikia.com", 
@@ -18,6 +19,7 @@ var wiki = new utility.Wiki({
     "debug": true
   }
 });
+*/
 var wikia = new utility.Wikia();
 
 var WIKIA_LOGO = 'http://img2.wikia.nocookie.net/__cb66/asoiaf/zh/images/8/89/Wiki-wordmark.png';
@@ -41,6 +43,12 @@ app.use('', wechat(wcConf, function(req, res, next) {
         res.reply('暂未找到相关词条。关键词功能正在开发完善，尽情期待！');
       }
     });
+  } else if (msg.MsgType == 'event') {
+    if (msg.Event == 'subscribe') {
+      res.reply('感谢订阅冰与火之歌中文维基。本账号如同一部随身携带的词典，帮助您轻松查阅冰火世界的名词释义。丹妮、史塔克、君临，键入任何感兴趣的名词、领略冰火世界的奇幻风光。');
+    } else if (msg.Event == 'unsubscribe') {
+      // TODO
+    }
   }
 }));
 
