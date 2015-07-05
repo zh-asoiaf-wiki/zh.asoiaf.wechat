2393a33fa55029ff73d193194c85a7a3b0eba158
chenchenyangll <ccy-8888@hotmail.com>
2015-07-05 18:19:55 +0800

    add feature: randomly pick MSG_ERR & MSG_NORESULT

--- asoiaf.js
+++ asoiaf.js
@@ -1,16 +1,8 @@
 var _ = require('lodash');
 var WeChat = require('node-huiji').WeChat;
-var config = require('./config.js');
-var wechat = new WeChat(config);
-
-// hack & keyword
-var hack = require('./hack.js');
-var keyword = require('./keyword.js');
-wechat.addHack(hack);
-wechat.addKeyword(keyword);
 
 // customize filter
-wechat._filter = function(results) {
+WeChat.prototype._filter = function(results) {
   var reg_filter = /(^用户博客|^TV talk|@comment)/;
   return _.filter(results, function(msg) {
     var qualified = !reg_filter.test(msg.title);    // filter legacy namespaces
@@ -23,5 +15,26 @@ wechat._filter = function(results) {
     return qualified;
   });
 };
+
+// customize _err & _noresult
+WeChat.prototype._err = function(err, res) {
+  console.log(err);
+  res.reply(_.sample(this.conf.CONST.MSG_ERR));
+  return;
+};
+WeChat.prototype._noresult = function(res) {
+  console.log('NO RESULT');
+  res.reply(_.sample(this.conf.CONST.MSG_NORESULT));
+  return;
+};
+
+var config = require('./config.js');
+var wechat = new WeChat(config);
+
+// hack & keyword
+var hack = require('./hack.js');
+var keyword = require('./keyword.js');
+wechat.addHack(hack);
+wechat.addKeyword(keyword);
 
 wechat.start();
