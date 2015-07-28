f56bc5513a6299f68d31e799bec26df2e4bdc27f
chenchenyangll <ccy-8888@hotmail.com>
2015-07-29 01:12:40 +0800

    remove customized filter: already push onto node-huiji, see HuijiWiki/node-huiji#6

--- asoiaf.js
+++ asoiaf.js
@@ -1,21 +1,6 @@
 var _ = require('lodash');
 var WeChat = require('node-huiji').WeChat;
 
-// customize filter
-WeChat.prototype._filter = function(results) {
-  var reg_filter = /(^用户博客|^TV talk|@comment)/;
-  return _.filter(results, function(msg) {
-    var qualified = !reg_filter.test(msg.title);    // filter legacy namespaces
-    if (qualified) {
-      var desc = msg.description;
-      var index = desc.indexOf('↑');
-      if (index >= 0)
-        msg.description = desc.substring(0, index); // filter tailing ref text
-    }
-    return qualified;
-  });
-};
-
 var config = require('./config.js');
 var wechat = new WeChat(config);
 
